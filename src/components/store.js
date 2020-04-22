55b9a59c44b7307f3a0c23d32275b02c915c4573
Ben <benclark001@gmail.com>
2020-04-22 18:04:24 +1000

    add generic contract update method

---
+++ src/components/store.js
@@ -0,0 +1,31 @@
+/**
+ * Save a contract
+ * @param {String} 		ref 		The contract reference ID
+ * @param {Object} 		data 		The data to save to the database
+ */
+export async function store(id, data) {
+	console.info(`Saving contract #${id}:`, data);
+
+	return new Promise((resolve, reject) => {
+		fetch(`${window.api_host}/v1/contract/${id}`, {
+			crossDomain: true,
+			method: 'PATCH',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body : JSON.stringify(data)
+		})
+		.then(response => { 
+			if (response.ok) {
+				resolve();
+			} else {
+				throw new Error(response.statusText);
+			}
+		})
+		.catch(error => {
+			console.error(error);
+			reject();
+		});
+	});
+}
